c56813b1d6c9dc9d72f6e6e8e79a6be1bea22ef5
leejunghyeok <wndgur76@naver.com>
2022-11-25 11:41:43 +0900

    her

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use('/api', (req, res, next)=>{
 });
 app.use('/api', apiRouter);
 // app.use('/users', usersRouter);
-app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next){
+// app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next){
+app.get("*", function(req, res, next){
     res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
 });
 
