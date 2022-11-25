3520f0778d7b94c45cf514425fc0b67826469b4b
leejunghyeok <wndgur76@naver.com>
2022-11-25 12:03:52 +0900

    her

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var apiRouter = require('./app_api/routes/index');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'app_server', 'views'));
-// app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
